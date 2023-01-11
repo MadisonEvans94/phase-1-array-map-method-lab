1a3c68a2885a2da19f49bb3285d705a90f89dd39
Madison Evans <m.corbinevans@gmail.com>
2023-01-11 08:58:33 -0700

    commit tree 7a1a412928a0f93fc87cf1de230f379f474d4977

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+	"what does the this keyword mean?",
+	"What is the Constructor OO pattern?",
+	"implementing Blockchain Web API",
+	"The Test Driven Development Workflow",
+	"What is NaN and how Can we Check for it",
+	"What is the difference between stopPropagation and preventDefault?",
+	"Immutable State and Pure Functions",
+	"what is the difference between == and ===?",
+	"what is the difference between event capturing and bubbling?",
+	"what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = (tutorials) => {
+	function upperCase(str) {
+		const stirArray = str.split(" ");
+
+		const upper = stirArray.map(
+			(entry) => entry[0].toUpperCase() + entry.slice(1)
+		);
+		return upper.join(" ");
+	}
+	return tutorials.map((tutorial) => upperCase(tutorial));
+};
